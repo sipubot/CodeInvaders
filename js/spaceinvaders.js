1285752d4e10fb8682708d938891bbe42c37590a
sipubot <ddasik00@naver.com>
2016-09-22 16:50:09 +0900

    ..

--- js/spaceinvaders.js
+++ js/spaceinvaders.js
@@ -5,7 +5,7 @@
 
 */
 
-/*  
+/*
     Game Class
 
     The Game class represents a Space Invaders game.
@@ -65,6 +65,8 @@ function Game() {
     this.sounds = null;
 }
 
+function 
+
 //  Initialis the Game with a canvas.
 Game.prototype.initialise = function(gameCanvas) {
 
@@ -85,18 +87,18 @@ Game.prototype.initialise = function(gameCanvas) {
 };
 
 Game.prototype.moveToState = function(state) {
- 
+
    //  If we are in a state, leave it.
    if(this.currentState() && this.currentState().leave) {
      this.currentState().leave(game);
      this.stateStack.pop();
    }
-   
+
    //  If there's an enter function for the new state, call it.
    if(state.enter) {
      state.enter(game);
    }
- 
+
    //  Set the current state.
    this.stateStack.pop();
    this.stateStack.push(state);
@@ -147,7 +149,7 @@ function GameLoop(game) {
 
         //  Get the drawing context.
         var ctx = this.gameCanvas.getContext("2d");
-        
+
         //  Update if we have an update function. Also draw
         //  if we have a draw function.
         if(currentState.update) {
@@ -231,12 +233,15 @@ WelcomeState.prototype.draw = function(game, dt, ctx) {
 
     ctx.font="30px Arial";
     ctx.fillStyle = '#ffffff';
-    ctx.textBaseline="center"; 
-    ctx.textAlign="center"; 
-    ctx.fillText("Space Invaders", game.width / 2, game.height/2 - 40); 
+    ctx.textBaseline="center";
+    ctx.textAlign="center";
+    ctx.fillText("Code Invaders", game.width / 2, game.height/2 - 40);
     ctx.font="16px Arial";
+    ctx.fillText("Select Stage", game.width / 2, game.height/2);
 
-    ctx.fillText("Press 'Space' to start.", game.width / 2, game.height/2); 
+    ctx.strokeStyle  = '#ffffff';
+
+    ctx.strokeRect(game.width / 1.5, game.height / 1.5, game.width / 5, game.height / 5);
 };
 
 WelcomeState.prototype.keyDown = function(game, keyCode) {
@@ -264,13 +269,13 @@ GameOverState.prototype.draw = function(game, dt, ctx) {
 
     ctx.font="30px Arial";
     ctx.fillStyle = '#ffffff';
-    ctx.textBaseline="center"; 
-    ctx.textAlign="center"; 
-    ctx.fillText("Game Over!", game.width / 2, game.height/2 - 40); 
+    ctx.textBaseline="center";
+    ctx.textAlign="center";
+    ctx.fillText("Game Over!", game.width / 2, game.height/2 - 40);
     ctx.font="16px Arial";
     ctx.fillText("You scored " + game.score + " and got to level " + game.level, game.width / 2, game.height/2);
     ctx.font="16px Arial";
-    ctx.fillText("Press 'Space' to play again.", game.width / 2, game.height/2 + 40);   
+    ctx.fillText("Press 'Space' to play again.", game.width / 2, game.height/2 + 40);
 };
 
 GameOverState.prototype.keyDown = function(game, keyCode) {
@@ -338,7 +343,7 @@ PlayState.prototype.enter = function(game) {
 };
 
 PlayState.prototype.update = function(game, dt) {
-    
+
     //  If the left or right arrow keys are pressed, move
     //  the ship. Check this on ticks rather than via a keydown
     //  event for smooth movement, otherwise the ship would move
@@ -432,7 +437,7 @@ PlayState.prototype.update = function(game, dt) {
     if(hitBottom) {
         this.lives = 0;
     }
-    
+
     //  Check for rocket/invader collisions.
     for(i=0; i<this.invaders.length; i++) {
         var invader = this.invaders[i];
@@ -443,7 +448,7 @@ PlayState.prototype.update = function(game, dt) {
 
             if(rocket.x >= (invader.x - invader.width/2) && rocket.x <= (invader.x + invader.width/2) &&
                 rocket.y >= (invader.y - invader.height/2) && rocket.y <= (invader.y + invader.height/2)) {
-                
+
                 //  Remove the rocket, set 'bang' so we don't process
                 //  this rocket again.
                 this.rockets.splice(j--, 1);
@@ -477,7 +482,7 @@ PlayState.prototype.update = function(game, dt) {
         var chance = this.bombRate * dt;
         if(chance > Math.random()) {
             //  Fire!
-            this.bombs.push(new Bomb(invader.x, invader.y + invader.height / 2, 
+            this.bombs.push(new Bomb(invader.x, invader.y + invader.height / 2,
                 this.bombMinVelocity + Math.random()*(this.bombMaxVelocity - this.bombMinVelocity)));
         }
     }
@@ -491,13 +496,13 @@ PlayState.prototype.update = function(game, dt) {
             game.lives--;
             game.sounds.playSound('explosion');
         }
-                
+
     }
 
     //  Check for invader/ship collisions.
     for(var i=0; i<this.invaders.length; i++) {
         var invader = this.invaders[i];
-        if((invader.x + invader.width/2) > (this.ship.x - this.ship.width/2) && 
+        if((invader.x + invader.width/2) > (this.ship.x - this.ship.width/2) &&
             (invader.x - invader.width/2) < (this.ship.x + this.ship.width/2) &&
             (invader.y + invader.height/2) > (this.ship.y - this.ship.height/2) &&
             (invader.y - invader.height/2) < (this.ship.y + this.ship.height/2)) {
@@ -524,7 +529,7 @@ PlayState.prototype.draw = function(game, dt, ctx) {
 
     //  Clear the background.
     ctx.clearRect(0, 0, game.width, game.height);
-    
+
     //  Draw ship.
     ctx.fillStyle = '#999999';
     ctx.fillRect(this.ship.x - (this.ship.width / 2), this.ship.y - (this.ship.height / 2), this.ship.width, this.ship.height);
@@ -589,10 +594,10 @@ PlayState.prototype.keyUp = function(game, keyCode) {
 };
 
 PlayState.prototype.fireRocket = function() {
-    //  If we have no last rocket time, or the last rocket time 
+    //  If we have no last rocket time, or the last rocket time
     //  is older than the max rocket rate, we can fire.
     if(this.lastRocketTime === null || ((new Date()).valueOf() - this.lastRocketTime) > (1000 / this.config.rocketMaxFireRate))
-    {   
+    {
         //  Add a rocket.
         this.rockets.push(new Rocket(this.ship.x, this.ship.y - 12, this.config.rocketVelocity));
         this.lastRocketTime = (new Date()).valueOf();
@@ -627,7 +632,7 @@ PauseState.prototype.draw = function(game, dt, ctx) {
     return;
 };
 
-/*  
+/*
     Level Intro State
 
     The Level Intro state shows a 'Level X' message and
@@ -646,12 +651,12 @@ LevelIntroState.prototype.update = function(game, dt) {
     }
     this.countdown -= dt;
 
-    if(this.countdown < 2) { 
-        this.countdownMessage = "2"; 
+    if(this.countdown < 2) {
+        this.countdownMessage = "2";
     }
-    if(this.countdown < 1) { 
-        this.countdownMessage = "1"; 
-    } 
+    if(this.countdown < 1) {
+        this.countdownMessage = "1";
+    }
     if(this.countdown <= 0) {
         //  Move to the next level, popping this state.
         game.moveToState(new PlayState(game.config, this.level));
@@ -666,17 +671,17 @@ LevelIntroState.prototype.draw = function(game, dt, ctx) {
 
     ctx.font="36px Arial";
     ctx.fillStyle = '#ffffff';
-    ctx.textBaseline="middle"; 
-    ctx.textAlign="center"; 
+    ctx.textBaseline="middle";
+    ctx.textAlign="center";
     ctx.fillText("Level " + this.level, game.width / 2, game.height/2);
     ctx.font="24px Arial";
-    ctx.fillText("Ready in " + this.countdownMessage, game.width / 2, game.height/2 + 36);      
+    ctx.fillText("Ready in " + this.countdownMessage, game.width / 2, game.height/2 + 36);
     return;
 };
 
 
 /*
- 
+
   Ship
 
   The ship has a position and that's about it.
@@ -712,11 +717,11 @@ function Bomb(x, y, velocity) {
     this.y = y;
     this.velocity = velocity;
 }
- 
-/*
-    Invader 
 
-    Invader's have position, type, rank/file and that's about it. 
+/*
+    Invader
+
+    Invader's have position, type, rank/file and that's about it.
 */
 
 function Invader(x, y, rank, file, type) {
--- js/starfield.js
+++ js/starfield.js
@@ -3,6 +3,28 @@
 
 */
 
+function StarfieldImages() {
+}
+
+StarfieldImages.images = function() {
+	this.imagelist = [
+		'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAD1BMVEUAAAD///9VVVUAAACqqqpggSv+AAAAAXRSTlMAQObYZgAAAEtJREFUCNd1jcENgDAMAy02cJsBaMIAICbI/kthRc2Te1nWycbhBXAlhZ1wqxBYybHIgDPnsxsFNXIsdyP+nNnOaKeW+yv6/QbeAh/+7w6qn7DhWwAAAABJRU5ErkJggg==',
+		'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAD1BMVEUAAAD///9VVVUAAACqqqpggSv+AAAAAXRSTlMAQObYZgAAAExJREFUCNd1y8ENwCAMA8CIDVxlgOIwQCsmyP5LkUTwxB9bJ1kyJKubQ98aCrUtsIsgpZEdKUOj/pDuOUKoeI7YPFKv4cC+Vz6RWZEFlLYLYltjut4AAAAASUVORK5CYII=',
+		'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAD1BMVEUAAAD///9VVVUAAACqqqpggSv+AAAAAXRSTlMAQObYZgAAADhJREFUCNdjIAYwKYGBA4OiIBgoM6iAaRFlBkVnY0EhiIihkpAiSMTRUMlJEUkNQheTMRgY4LEKACepCGT9ISzjAAAAAElFTkSuQmCC'
+	];
+
+	var imageFilejson = "images.json";
+	var images = [this.imagelist.length];
+	for(var i=0; i<this.imagelist.length; i++) {
+		images[i] = new Image();
+		images[i].src = 'data:image/png;base64,' + this.imagelist[i];
+	}
+	this.images = images;
+};
+
+StarfieldImages.images();
+//StarfieldImages.images();
+
 //	Define the starfield class.
 function Starfield() {
 	this.fps = 30;
@@ -18,7 +40,6 @@ function Starfield() {
 //	The main function - initialises the starfield.
 Starfield.prototype.initialise = function(div) {
 	var self = this;
-
 	//	Store the div.
 	this.containerDiv = div;
 	self.width = window.innerWidth;
@@ -30,7 +51,7 @@ Starfield.prototype.initialise = function(div) {
 		self.canvas.width = self.width;
 		self.canvas.height = self.height;
 		self.draw();
- 	}
+ 	};
 
 	//	Create the canvas.
 	var canvas = document.createElement('canvas');
@@ -45,8 +66,8 @@ Starfield.prototype.start = function() {
 	//	Create the stars.
 	var stars = [];
 	for(var i=0; i<this.stars; i++) {
-		stars[i] = new Star(Math.random()*this.width, Math.random()*this.height, Math.random()*3+1,
-		 (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity);
+		stars[i] = new Star(Math.random()*this.width, Math.random()*this.height, Math.random()*20+1,
+		 (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity, Math.floor(Math.random()*3));
 	}
 	this.stars = stars;
 
@@ -54,7 +75,7 @@ Starfield.prototype.start = function() {
 	//	Start the timer.
 	this.intervalId = setInterval(function() {
 		self.update();
-		self.draw();	
+		self.draw();
 	}, 1000 / this.fps);
 };
 
@@ -70,8 +91,8 @@ Starfield.prototype.update = function() {
 		star.y += dt * star.velocity;
 		//	If the star has moved from the bottom of the screen, spawn it at the top.
 		if(star.y > this.height) {
-			this.stars[i] = new Star(Math.random()*this.width, 0, Math.random()*3+1, 
-		 	(Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity);
+			this.stars[i] = new Star(Math.random()*this.width, 0, Math.random()*20+1,
+		 	(Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity, Math.floor(Math.random()*3));
 		}
 	}
 };
@@ -89,13 +110,14 @@ Starfield.prototype.draw = function() {
 	ctx.fillStyle = '#ffffff';
 	for(var i=0; i<this.stars.length;i++) {
 		var star = this.stars[i];
-		ctx.fillRect(star.x, star.y, star.size, star.size);
+		ctx.drawImage(StarfieldImages.images[star.imageNum], star.x, star.y, star.size, star.size);
 	}
 };
 
-function Star(x, y, size, velocity) {
+function Star(x, y, size, velocity, imageNum) {
 	this.x = x;
-	this.y = y; 
+	this.y = y;
 	this.size = size;
 	this.velocity = velocity;
+	this.imageNum = imageNum;
 }
